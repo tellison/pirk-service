d181f5e9ff8939cbc2a3870a4bcab83b12466609
Tim Ellison <t.p.ellison@gmail.com>
2016-10-24 12:28:05 +0100

    Full conversion of swagger PUT to Pirk query

--- src/main/java/com/peir/pirk/QueryController.java
+++ src/main/java/com/peir/pirk/QueryController.java
@@ -1,49 +1,55 @@
 package com.peir.pirk;
 
+import java.io.IOException;
 import java.math.BigInteger;
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.UUID;
 
 import javax.ws.rs.core.Response;
 
+import org.apache.pirk.query.wideskies.Query;
 import org.apache.pirk.query.wideskies.QueryInfo;
 import org.apache.pirk.schema.query.QuerySchema;
-import org.apache.pirk.schema.query.filter.DataFilter;
-import org.apache.pirk.schema.query.filter.FilterFactory;
+import org.apache.pirk.schema.query.QuerySchemaBuilder;
+import org.apache.pirk.schema.query.QuerySchemaRegistry;
+import org.apache.pirk.utils.PIRException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import io.swagger.model.Query;
 
 public class QueryController {
 
     private static final Logger logger = LoggerFactory.getLogger(QueryController.class);
 
+    //FIXME: temp store
+    static Map<String, Query> store = new HashMap<>();
+    
     // PUT /v1/queries/{id}
-    public static Response queriesIdPut(String id, Query query) {
+    public static Response queriesIdPut(String id, io.swagger.model.Query query) {
         logger.info("queriesIdPut");
 
         System.out.println("queriesPut");
 
         /* Construct a Pirk query from incoming swagger model query. */
 
-        //TODO: shift all this schema building into the core pirk capability
-        
-        DataFilter filter = FilterFactory.getFilter(query.getQuerySchema().getFilter(), new HashSet<String>(query.getQuerySchema().getFilteredFields()));
-        //FIXME: Should be computed from data elements 
-        int dataElementSize = 0;
-        // Query schema
-        QuerySchema qs = new QuerySchema(
-            query.getQuerySchema().getName(),
-            query.getQuerySchema().getDataSchemaName(),
-            query.getQuerySchema().getPrimarySelector(),
-            query.getQuerySchema().getFilter(),
-            filter,
-            dataElementSize);
-        
-        
+        Query pirkQuery;
+        try {
+          pirkQuery =  buildPirkQuery(query);
+          store.put(query.getId(), pirkQuery);
+        } catch (PIRException e) {
+            e.printStackTrace();
+        }
+
+        return Response.ok(query.getId()).build();
+    }
+
+    private static Query buildPirkQuery(io.swagger.model.Query query) throws PIRException {
+        // Extract the query schema
+        QuerySchema pirkSchema = definePirkSchema(query);
+
         // RSA modulus
         BigInteger modulus = parseBigInteger(query.getModulus());
 
@@ -54,53 +60,47 @@ public class QueryController {
             queryElements.put(index++, parseBigInteger(element));
         }
 
-        QueryInfo qi = new QueryInfo(
-            UUID.fromString(query.getId()),
-            query.getNumSelectors(),
+        // Build a PIR query info object.
+        QueryInfo queryInfo = new QueryInfo(
+            UUID.fromString(query.getId()), 
+            query.getNumSelectors(), 
             query.getHashBitSize(),
             query.getHashKey(),
             query.getDataPartitionBitSize(),
-            "querySchemaName",
-            true, false, false);
+            pirkSchema.getSchemaName(), true, false, false);
+        
+        return new Query(queryInfo, modulus, queryElements);
+    }
 
-        org.apache.pirk.query.wideskies.Query pirkQuery = new org.apache.pirk.query.wideskies.Query(qi, modulus,
-                queryElements);
-        // QuerySchema qs = new QuerySchema();
-        // List<String> queryElements = new ArrayList<>();
-        // Query q = new Query();
-        //
-        // q.setId(id);
-        // q.setNumSelectors(2);
-        // q.setHashBitSize(12);
-        // q.setHashKey("Foobar");
-        // q.setNumBitsPerDataElement(96);
-        // q.setDataPartitionBitSize(8);
-        // q.setNumPartitionsPerDataElement(12);
-        // q.setModulus(
-        // "30049399560646469676005493659082752898785212655599017144565972358099446280042588990477674146012309524890531874672789");
-        // q.setQuerySchema(qs);
-        // q.setQueryElements(queryElements);
-        //
-        // qs.setDataSchemaName("simple query");
-        // qs.setDataSchemaName("Simple Data Schema 1");
-        // qs.setElementNames(Arrays.asList("name", "age"));
-        // qs.setFilter("com.example.Foo");
-        // qs.setFilteredFields(Arrays.asList("filtered_field"));
-        //
-        // queryElements.add(
-        // "191550723406084518591469029284995656590807890583952791477054646763013652531022594807624560411348538761707449665459721391970132574471789554192515615653292243752775621542596319777517406846487590458619682935632633366194785222228749954");
-        // queryElements.add(
-        // "708761260223492284214606189608835350828740716107713085191175122749726459266750635699507731862886370744878661635914854481831914893579356324072737130381958968722184499314288762113389523795210662995774011539282843855788464474546164091");
+    private static QuerySchema definePirkSchema(io.swagger.model.Query query) throws PIRException {
+        io.swagger.model.QuerySchema swaggerSchema = query.getQuerySchema();
+        QuerySchema pirkSchema = QuerySchemaRegistry.get(swaggerSchema.getName());
 
-        return Response.ok().build();
+        if (pirkSchema != null) {
+            logger.info("Query schema {} is already defined, using that one");
+        } else {
+            QuerySchemaBuilder schemaBuilder = new QuerySchemaBuilder();
+            schemaBuilder.setName(swaggerSchema.getName());
+            schemaBuilder.setDataSchemaName(swaggerSchema.getDataSchemaName());
+            schemaBuilder.setQueryElements(new LinkedHashSet<String>(swaggerSchema.getElementNames()));
+            schemaBuilder.setSelectorName(swaggerSchema.getPrimarySelector());
+            schemaBuilder.setFilterTypeName(swaggerSchema.getFilter());
+            try {
+                pirkSchema = schemaBuilder.build();
+            } catch (IOException e) {
+                throw new PIRException(e.getLocalizedMessage());
+            }
+        }
+        return pirkSchema;
     }
 
     private static BigInteger parseBigInteger(String modulusString) {
         try {
-            return new BigInteger(modulusString, Character.MAX_RADIX);
+            // TODO return new BigInteger(modulusString, Character.MAX_RADIX);
+            return new BigInteger(modulusString);
         } catch (NumberFormatException e) {
-            //TODO throw new ApiException(400, e.getLocalizedMessage());
-          return BigInteger.ZERO;
+            // TODO throw new ApiException(400, e.getLocalizedMessage());
+            return BigInteger.ZERO;
         }
     }
 
